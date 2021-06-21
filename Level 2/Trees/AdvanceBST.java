886770620e68aa385fed925f68e2023bffad654f
Faisal Imtyaz <faisal23.imtyaz@gmail.com>
2021-06-22 05:26:39 +0530

    update

--- Level 2/Trees/AdvanceBST.java
+++ Level 2/Trees/AdvanceBST.java
@@ -108,4 +108,112 @@ public class AdvanceBST {
 		}
 	}
 
+	//contruction series ``````````````````````````````````````````````````````````````````````````````````````````````
+
+	//construct from inorder L-N-R
+	public static TreeNode bstFromInorder(int[] inorder) {
+		return bstFromInorder_(inorder, 0, inorder.length - 1);
+
+	}
+
+	public static TreeNode bstFromInorder_(int[] inorder , int st , int end) {
+		if (st > end)return null;
+		int mid = st + (end - st) / 2;
+
+		TreeNode root = new TreeNode(inorder[mid]);
+
+		root.left = bstFromInorder_(inorder, st, mid - 1);
+		root.right = bstFromInorder_(inorder, mid + 1, end);
+
+		return root;
+	}
+
+	//construct from preorder N-L-R using ranges
+	public static TreeNode bstFromPreorder(int[] preorder) {
+		return bstFromPreorder_(preorder, -(int)1e9, (int)1e9);
+
+	}
+
+	static int idx = 0 ;
+	public static TreeNode bstFromPreorder_(int[] preorder , int l , int r) {
+		if (idx == preorder.length || preorder[idx] < l || preorder[idx] > r)
+			return null;
+
+		TreeNode node = new TreeNode(preorder[idx++]);
+
+
+		node.left = bstFromPreorder_(preorder, l, node.val);
+		node.right = bstFromPreorder_(preorder, node.val, r);
+
+		return node;
+
+	}
+
+	// in post-order same technique , idx start from postorder.length-1 to 0
+	// call L-R-Root right call first , then left call
+	//in traversing from back root comes first -> right -> left
+	static int idx;
+	public static TreeNode bstFromPostorder(int[] postorder) {
+		idx = postorder.length - 1;
+		return bstFromPostorder_(postorder, -(int)1e9, (int)1e9);
+
+	}
+
+
+	public static TreeNode bstFromPostorder_(int[] postorder , int l , int r) {
+		if (idx < 0 || postorder[idx] < l || postorder[idx] > r)
+			return null;
+
+		TreeNode node = new TreeNode(postorder[idx--]);
+
+
+		node.right = bstFromPostorder_(postorder, node.val, r);
+		node.left = bstFromPostorder_(postorder, l, node.val);
+
+		return node;
+
+	}
+
+	//BST From Level Order
+
+	public static class bstPair{
+		TreeNode node = null;
+		int lr = 0;
+		int rr = 0;
+
+		bstPair() {}
+
+		bstPair(TreeNode node , int lr , int rr ) {
+			this.node = node;
+			this.lr = lr;
+			this.rr = rr;
+		}
+	}
+
+
+	static int idx;
+	public static TreeNode bstFromLevelOrder(int[] levelorder) {
+		idx = 0 ;
+		return bstFromPostorder_(levelorder, -(int)1e9, (int)1e9);
+
+	}
+
+
+	public static TreeNode bstFromLevelOrderr_(int[] levelorder , int l , int r) {
+		if (idx == levelorder.length || levelorder[idx] < l || levelorder[idx] > r)
+			return null;
+
+
+		TreeNode node = find(levelorder, idx++, l, r);
+
+		node.right = bstFromLevelOrder_(levelorder, node.val, r);
+		node.left = bstFromLevelOrder_(levelorder, l, node.val);
+
+		return node;
+
+	}
+
+
+
+
 }
