aa08edb3f7aa7e6fb2ba0af47faf06f53e3e0dbc
fai-98 <75186248+fai-98@users.noreply.github.com>
2022-12-06 00:53:47 +0530

    Time: 26 ms (50.23%), Space: 75.7 MB (49.77%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.java
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -1,31 +1,25 @@
 class Solution {
     public int minimumAverageDifference(int[] nums) {
         int n = nums.length;
-        long[] pre = new long[n];
-        long[] suf = new long[n];
+        long sum = 0;
         
-        pre[0] = nums[0];
-        suf[n-1] = nums[n-1];
-        
-        for(int i=1; i<n; i++){
-            pre[i] = pre[i-1] + nums[i];
+        for(int i=0; i<n; i++){
+           sum += nums[i];
         }
         
-        for(int i=n-2; i>=0; i--){
-            suf[i] = suf[i+1] + nums[i];
-        }
-        
-        long min = (int)1e18;
+        long min = (int)1e18, pre = 0, suf = 0;
         int idx = -1;
         
         for(int i=0; i<n; i++){
             long avg = 0;
+            pre += nums[i];
+            suf = sum - pre;
                 
             if(i == n-1){
-                avg = pre[i]/(i+1);
+                avg = pre/(i+1);
             }else{
-                long avg1 = pre[i]/(i+1);
-                long avg2 = suf[i+1]/(n-i-1);
+                long avg1 = pre/(i+1);
+                long avg2 = suf/(n-i-1);
                 
                 avg = Math.abs(avg1-avg2);
             }
